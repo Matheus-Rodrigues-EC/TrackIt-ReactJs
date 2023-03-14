8add77f469757103814a4e1c307e18b0a2d1e033
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-03-14 17:48:48 -0300

    Create Login Screen
    Criação da tela de Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Cadastro from "./Components/Cadastro";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+
+    <Routes>
+      <Route exact path={`/`} element={<Login/>}/>
+      <Route path={`/cadastro`} element={<Cadastro/>}/>
+    </Routes>
     
-    </>
+    </BrowserRouter>
   );
 }
 
